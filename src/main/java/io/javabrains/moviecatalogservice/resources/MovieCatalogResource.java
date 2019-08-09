2d9b591bc64c176afb7099a208044237d7765e21
sag sag <sohrabag@kth.se>
2019-08-09 15:29:15 +0200

    movie-catalog-service files added

---
+++ src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,24 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.moviecatalogservice.models.CatalogItems;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+	@RequestMapping("/{userid}")
+	public List<CatalogItems> getCatalog(@PathVariable("userid") String userId){
+		
+		return Collections.singletonList(
+				new CatalogItems("Transformers", "Test", 4)
+				);
+				
+	}
+}
